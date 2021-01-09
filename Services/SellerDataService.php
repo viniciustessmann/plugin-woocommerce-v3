c50b2d2485256d4d521479574bfd06a80e91c3f6
Vinícius Tessmann <vinicius.tessmann@melhorenvio.com>
2021-01-09 11:16:44 -0300

    Removendo token fixo

--- Services/SellerDataService.php
+++ Services/SellerDataService.php
@@ -8,24 +8,42 @@ namespace V3\Services;
  */
 class SellerDataService
 {
+    const KEY_SHIPPING_METHOD_TESSMANN = 'tessmann-shipping';
     /**
      * @return objects
      */
     public function get()
     {
-        $shipping_methods = \WC()->shipping->get_shipping_methods();
-        $data = null;
-        foreach($shipping_methods as $method) {
-            if($method->id == 'tessmann-shipping') {
-                unset($method->setting['token']);
-                $data = $method->settings;
-            }
-        }
-
+        $data = $this->getShippingMethodTessmann();
         $data['country_id'] = 'BR';
         unset($data['token']);
         unset($data['enabled']);
 
         return (object) $data;
+    }
+
+    /**
+     * @return string
+     */
+    public function getToken()
+    {
+        $data = $this->getShippingMethodTessmann();
+        return  $data['token'];
+    }
+
+    /**
+     * @return array
+     */
+    public function getShippingMethodTessmann()
+    {
+        $shipping_methods = \WC()->shipping->get_shipping_methods();
+        $data = null;
+        foreach($shipping_methods as $method) {
+            if($method->id == self::KEY_SHIPPING_METHOD_TESSMANN) {
+                $data = $method->settings;
+            }
+        }
+
+        return $data;
     }
 }
