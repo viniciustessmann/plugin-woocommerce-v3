33f8588b6f5283863743458e1d7067cbfdce587d
Vinícius Tessmann <vinicius.tessmann@melhorenvio.com>
2021-01-16 18:52:29 -0300

    ajustes

--- Services/RequestService.php
+++ Services/RequestService.php
@@ -5,9 +5,9 @@ namespace Tessmann\Services;
 
 class RequestService
 {
-    //const URL = 'https://api.melhorenvio.com/v2/me';
+    const URL = 'https://api.melhorenvio.com/v2/me';
 
-    const URL = 'https://sandbox.melhorenvio.com.br/api/v2/me';
+    //const URL = 'https://sandbox.melhorenvio.com.br/api/v2/me';
 
     const TIMEOUT = 600;
 
